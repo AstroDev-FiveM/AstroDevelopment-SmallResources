0e7185dff4f51dbfde2d9b738bd7f19bf5bcec24
katakume <72819680+katakume-astro@users.noreply.github.com>
2022-10-15 12:51:37 +0200

    Update config.lua

--- Astro-Dev.eu-smallresources/config.lua
+++ Astro-Dev.eu-smallresources/config.lua
@@ -13,7 +13,6 @@ Config.MechanikJob = 'mechanic' -- nazwa joba mechanika (mechanic/mecano...)
 Config.CelowaniePierwsza = false -- true - celowanie z pierwszej osoby, false - celowanie z trzeciej osoby
 Config.WylaczKropka = false -- Ustaw na false jeżeli chcesz mieć kropka podczas celowania 
 
-
 -- Hud z GTA V
 Config.PancerziZdrowie = true -- Ustaw na true jeżeli chcesz usunąć paski Pancerza i Zdrowia, ustaw na false jezeli chcesz je zostawić
 Config.Minimapa = true -- Ustaw na true jeżeli chcesz usunąć minmape kiedy gracz jest poza pojazdem
