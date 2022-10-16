b6c0386985b877b430d4f8d5773a71108cb82c79
katakume <72819680+katakume-astro@users.noreply.github.com>
2022-10-16 11:37:41 +0200

    nowe funkcje - częstotliwość fali na wodzie

--- Astro-Dev.eu-smallresources/client/snippets.lua
+++ Astro-Dev.eu-smallresources/client/snippets.lua
@@ -1,9 +1,19 @@
 Citizen.CreateThread(function()
-	if Config.Pociagi then
+    if Config.Pociagi then
     SwitchTrainTrack(0, true)
     SwitchTrainTrack(3, true)
     SetTrainTrackSpawnFrequency(0, 120000) 
     SetRandomTrains(1)
     SetTrainsForceDoorsOpen(false)
-	end
+    end
+end)
+
+CreateThread(function()
+    if Config.CustomoweFaleWoda then
+    local value = 0.5
+    while true do
+    WaterOverrideSetStrength(value)
+    Wait()
+    end
+    end
 end)
